0735ab196f0c65beca4eb6e0897998ffb040243f
sarahl1 <sarahli99@hotmail.com>
2019-04-07 11:28:43 -0700

    prompts for username and password

--- cred.js
+++ cred.js
@@ -1,4 +1,4 @@
 module.exports = {
-    username: 'USERNAME',
-    password: 'PASSWORD'
+    username: '',
+    password: ''
 }
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
+const readline = require('readline')
+const io = require('console-read-write');
 
 // puppeteer.launch().then(async browser => {
 //   const page = await browser.newPage();
 //   await page.goto('https://github.com');
-//   await page.authenticate("enter username", "enter password");
+//   await page.authenticate("", "");
 //   setTimeout('', 5000);
 //   await page.screenshot({path: 'screenshot.png'});
 //   await browser.close();
@@ -22,16 +24,21 @@ async function run() {
     const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';   
     const CREDS = require('./cred.js');
 
+    io.write("Enter username:");
+    const user = await io.read();
+    io.write("Enter password:");
+    const pass = await io.read();
+
     await page.click(USERNAME_SELECTOR);
-    await page.keyboard.type(CREDS.username);
+    await page.keyboard.type(user);
     await page.click(PASSWORD_SELECTOR);
-    await page.keyboard.type(CREDS.password);
+    await page.keyboard.type(pass);
     await page.click(BUTTON_SELECTOR);
 
     await page.waitForNavigation();
 
 
-    browser.close();
+    //browser.close();
   }
   
   run();
